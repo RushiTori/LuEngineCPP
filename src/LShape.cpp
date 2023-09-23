5cccf44c46df51e85b5c5eb39a37753c7f367962
RushiTori <arcturusarc09@gmail.com>
2023-09-23 18:30:03 +0200

    forgot to implement "SetSkin"

--- src/LShape.cpp
+++ src/LShape.cpp
@@ -1,11 +1,11 @@
 #include "LShape.hpp"
 
-void LShape::SetTexture(const Texture2D* tex) {
-  SetTexture(tex, 0, 0, tex->width, tex->height);
-}
+void LShape::SetTexture(const Texture2D* tex) { SetTexture(tex, 0, 0, tex->width, tex->height); }
 
-void LShape::SetTexture(const Texture2D* tex, uint x, uint y, uint w, uint h) {
-  this->skin = TexView(tex, x, y, w, h);
+void LShape::SetTexture(const Texture2D* tex, uint x, uint y, uint w, uint h) { this->skin = TexView(tex, x, y, w, h); }
+
+void LShape::SetSkin(const TexView& skin) {
+	if (skin.tex) this->skin = skin;
 }
 
 void LShape::Display() const {}
